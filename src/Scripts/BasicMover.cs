5cb386d04e4abef9207af281cfa8af711091345c
Mohamed Farag <mohamedxiv@gmail.com>
2022-04-18 13:18:53 +0200

    src Added

---
+++ src/Scripts/BasicMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    //private Transform gameobjectTransform;
+
+    private void Start()
+    {
+        //gameobjectTransform =  GetComponent<Transform>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        //Debug.Log(Time.timeSinceLevelLoad);
+    }
+}
